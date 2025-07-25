52c5bf426d68950e56d0fa26f624f0d2f56d4edf
CodingDad89 <lutz.amgarten@web.de>
2025-07-25 22:42:18 +0200

    different things implemented

--- portfolio/src/app/Header/header.component.ts
+++ portfolio/src/app/Header/header.component.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule, NavbarComponent],
+    imports: [CommonModule, NavbarComponent, HeroComponent],
     template: `
     <section>
         <app-navbar></app-navbar>
+        <app-hero></app-hero>
+
     </section>`,
 
                 
