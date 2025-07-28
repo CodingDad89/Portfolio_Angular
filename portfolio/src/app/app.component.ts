0fe156005ec284ca630ab150e5d073376b0e50da
CodingDad89 <lutz.amgarten@web.de>
2025-07-28 10:36:30 +0200

    A LOT OF CHANGES DONE
    New component about me
    New component header
    CSS changes fonts
    aligning everything

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent} from './Header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
