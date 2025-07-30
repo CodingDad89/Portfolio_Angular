fead15abc11ef6d74ba1c00f6ce8076d12a90336
CodingDad89 <lutz.amgarten@web.de>
2025-07-30 08:44:00 +0200

    Much more things done
    Arrows positioning
    animations
    picture size
    etc

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent} from './Header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ArrowComponent } from './arrow/arrow.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutMeComponent, ArrowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
